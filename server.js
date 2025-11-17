c1767507745ae477f5c90bbbc883788abe9cf46a
UserNotFound876404 <usernotfound876594@gmail.com>
2025-11-17 17:04:02 +0800

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
     next();
 })
 
-const db = client.db(dbName);
+
 const users = new Array(
     { name: 'admin', password: 'admin' }
 );
@@ -50,15 +50,15 @@ const insertDatabase = async (db, object) => {
 }
 
 //update mongodb
-const updateDatabase = async (db, oldName, newName, age, weight, height, meds, userId) => {
+const updateDatabase = async (db, oldUserId, newName, age, weight, height, meds, userId, gender) => {
     var collection = db.collection(collectionName);
-    collection.updateMany({ 'name': oldName }, { $set: { 'name': newName, 'weight': weight, 'height': height, 'medicine': meds, 'userId': userId, 'age': age } });
+    collection.updateMany({ 'userId': oldUserId }, { $set: { 'name': newName, 'weight': weight, 'height': height, 'medicine': meds, 'userId': userId, 'age': age ,'gender':gender} });
 }
 
 //delete mongodb
-const deleteDatabase = async (db, name) => {
+const deleteDatabase = async (db, userId) => {
     var collection = db.collection(collectionName);
-    collection.deleteOne({ "name": name });
+    collection.deleteOne({ "userId": userId });
 }
 
 app.get('/', (req, res, next) => {
@@ -77,11 +77,23 @@ app.get('/login', (req, res, next) => {
     res.status(200).render('login');
 });
 
+app.get('/signup',(req,res,next)=>{
+    res.status(200).render('signup');
+})
+app.post('/signup',(req,res,next)=>{
+    let newAccount = {};
+    newAccount['name']= req.body.name;
+    newAccount['password']=req.body.password;
+    users.push(newAccount);
+    res.redirect("login");
+});
+
 //insert
 app.get('/insert', (req, res, next) => {
     res.status(200).render('insert');
 })
 app.post("/insert", async (req, res, next) => {
+    const db = client.db(dbName);
     try {
         let newObject = {};
         newObject['userId'] = req.body.userId;
@@ -90,6 +102,7 @@ app.post("/insert", async (req, res, next) => {
         newObject['weight'] = req.body.weight;
         newObject['height'] = req.body.height;
         newObject['medicine'] = req.body.medicine;
+        newObject['gender'] = req.body.gender;
         insertDatabase(db, newObject);
         res.status(200).redirect("success"); // sends JSON and ends response
     } catch (err) {
@@ -101,12 +114,29 @@ app.post("/insert", async (req, res, next) => {
 //update
 app.get('/update', (req, res, next) => {
     res.status(200).render('update');
-})
+});
 app.post("/update", async (req, res, next) => {
+    const db = client.db(dbName);
     try {
-        updateDatabase(db, req.body.oldName, req.body.name, req.body.age, req.body.weight, req.body.height, req.body.medicine, req.body.userId);
+        updateDatabase(db, req.body.olduserId, req.body.name, req.body.age, req.body.weight, req.body.height, req.body.medicine, req.body.userId,req.body.gender);
         res.status(200).redirect("success");
     } catch (err) {
+        console.error("Error fetching database:", err);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+//delete
+app.get('/delete', (req, res, next) => {
+    res.status(200).render('delete');
+});
+app.post('/delete',(req,res,next)=>{
+    const db = client.db(dbName); 
+    try{
+        deleteDatabase(db,req.body.userId);
+        res.status(200).redirect("success");
+    }
+    catch (err){
         console.error("Error fetching database:", err);
         res.status(500).json({ error: "Internal server error" });
     }
@@ -118,6 +148,10 @@ app.get('/success', (req, res, next) => {
 
 app.get('/fail', (req, res) => {
     res.status(200).render('fail');
+})
+
+app.get('/about',(req,res,next)=>{
+    res.status(200).render("about");
 })
 
 app.post('/login', (req, res) => {
@@ -136,6 +170,7 @@ app.get('/list', async (req, res) => {
         res.redirect('/login');
     } else {
         try {
+            const db = client.db(dbName);
             const data = await findDatabase(db); // wait for data
             res.status(200).render('list', { name: req.session.username, num: data.length, data: data });
         } catch (error) {
@@ -155,6 +190,7 @@ app.get('/logout', (req, res) => {
 //curl "localhost:8099/api"
 app.get("/api", async (req, res, next) => {
     try {
+        const db = client.db(dbName);
         const result = await findDatabase(db); // await the helper
         res.status(200).json(result); // sends JSON and ends response
     } catch (err) {
@@ -163,9 +199,8 @@ app.get("/api", async (req, res, next) => {
     }
 });
 
-//create
-//curl -X POST -d name=d1 "localhost:8099/api"
-//curl -X POST -d "name=test&age=40&userId=22222&weight=76&height=177&medicine=meds0&medicine=meds4" "localhost:8099/api"
+//insert
+//curl -X POST -d "name=test&age=40&userId=22222&weight=76&height=177&medicine=meds0&medicine=meds4&gender=Male" "localhost:8099/api"
 app.post("/api", async (req, res, next) => {
     try {
         let newObject = {};
@@ -175,9 +210,10 @@ app.post("/api", async (req, res, next) => {
         newObject['weight'] = req.body.weight;
         newObject['height'] = req.body.height;
         newObject['medicine'] = req.body.medicine;
-        insertDatabase(db, newObject);
-        let result = await findDatabase(db);
-        res.status(200).json(result); // sends JSON and ends response
+        newObject['gender'] = req.body.gender;
+        const db = client.db(dbName);
+        await insertDatabase(db, newObject);
+        res.status(200).send("Data inserted"); // sends JSON and ends response
     } catch (err) {
         console.error("Error fetching database:", err);
         res.status(500).json({ error: "Internal server error" });
@@ -185,13 +221,14 @@ app.post("/api", async (req, res, next) => {
 });
 
 //update
-//curl -X PUT -d "name=demo&age=20&userId=11111&weight=50&height=177&medicine=meds0&medicine=meds4&medicine=meds2" "localhost:8099/api/name/test"
-app.put("/api/name/:name", async (req, res, next) => {
+//curl -X PUT -d "name=demo&age=20&userId=11111&weight=50&height=177&medicine=meds0&medicine=meds4&medicine=meds2&gender=Male" "localhost:8099/api/userId/22222"
+app.put("/api/userId/:userId", async (req, res, next) => {
     try {
+        const db = client.db(dbName);
         const database = await findDatabase(db);
         database.forEach((object) => {
-            if (object.name == req.params.name) {
-                updateDatabase(db, req.params.name, req.body.name, req.body.age, req.body.weight, req.body.height, req.body.medicine, req.body.userId,);
+            if (object.userId == req.params.userId) {
+                updateDatabase(db, req.params.userId, req.body.name, req.body.age, req.body.weight, req.body.height, req.body.medicine, req.body.userId, req.body.gender);
             }
         })
         const result = await findDatabase(db);
@@ -204,17 +241,17 @@ app.put("/api/name/:name", async (req, res, next) => {
 })
 
 //delete
-//curl -X DELETE "localhost:8099/api/delete/name/demo"
-app.delete("/api/delete/name/:name", async (req, res, next) => {
+//curl -X DELETE "localhost:8099/api/delete/userId/11111"
+app.delete("/api/delete/userId/:userId", async (req, res, next) => {
     try {
+        const db = client.db(dbName);
         const database = await findDatabase(db);
         database.forEach((object) => {
-            if (object.name == req.params.name) {
-                deleteDatabase(db, req.params.name);
+            if (object.userId == req.params.userId) {
+                deleteDatabase(db, req.params.userId);
             }
         })
-        const result = await findDatabase(db);
-        res.status(200).type("json").json(result);
+        res.status(200).type("json").send("Data deleted");
     } catch (err) {
         console.error("Error fetching database:", err);
         res.status(500).json({ error: "Internal server error" });
